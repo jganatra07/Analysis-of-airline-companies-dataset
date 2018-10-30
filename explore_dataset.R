99afc8eda1580fa26b9d4fc3025478934ccd3859
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-30 11:50:57 -0400

    Saving explore_dataset.R

--- explore_dataset.R
+++ explore_dataset.R
@@ -12,7 +12,7 @@ summary(unclean_data$Flight.time.in.minutes)
 
 #Replacing the NA's
 unclean_data$Departure.Delay.in.Minutes[is.na(unclean_data$Departure.Delay.in.Minutes)] <- mean(unclean_data$Departure.Delay.in.Minutes, na.rm=TRUE)
-unclean_data$Arrival.Delay.in.Minutes[is.na(unclean_data$Departure.Delay.in.Minutes)] <- mean(unclean_data$Arrival.Delay.in.Minutes, na.rm=TRUE)
+unclean_data$Arrival.Delay.in.Minutes[is.na(unclean_data$Arrival.Delay.in.Minutes)] <- mean(unclean_data$Arrival.Delay.in.Minutes, na.rm=TRUE)
 unclean_data$Flight.time.in.minutes[is.na(unclean_data$Flight.time.in.minutes)] <- median(unclean_data$Flight.time.in.minutes, na.rm=TRUE)
 
 #Summary after removing NA's 
