2bfa0d6c00453378945e78313f090d02017d25c3
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-30 11:39:08 -0400

    Saving explore_dataset.R

--- explore_dataset.R
+++ explore_dataset.R
@@ -4,3 +4,8 @@ library(dplyr)
 
 unclean_data$Satisfaction<-as.numeric(unclean_data$Satisfaction)
 str(unclean_data)
+
+#Spread of the three variables with missing variables
+summary(unclean_data$Departure.Delay.in.Minutes)
+summary(unclean_data$Arrival.Delay.in.Minutes)
+summary(unclean_data$Flight.time.in.minutes)
