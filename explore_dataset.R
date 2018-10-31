5d99765dfcdaaac295c9bb7384cb3518edc613cd
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-31 14:01:42 -0400

    Saving explore_dataset.R

--- explore_dataset.R
+++ explore_dataset.R
@@ -1,3 +1,8 @@
 
 library(jsonlite)
 library(ggplot2)
+
+#
+flight_cancel_plot<-ggplot(satisfactionSurvey)+geom_bar(stat = "identity",aes(x=satisfactionSurvey$Flight.cancelled,y=satisfactionSurvey$Satisfaction),width = 0.4)
+flight_cancel_plot<-flight_cancel_plot+labs(x="Flight Cancellation",y="Customer Satisfaction")
+flight_cancel_plot
