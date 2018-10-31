e8163731b332c147a926f9b90d784be669ca7b22
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-31 16:16:24 -0400

    Saving explore_dataset.R

--- explore_dataset.R
+++ explore_dataset.R
@@ -16,17 +16,9 @@ unclean_data$Departure.Delay.in.Minutes[is.na(unclean_data$Departure.Delay.in.Mi
 unclean_data$Arrival.Delay.in.Minutes[is.na(unclean_data$Arrival.Delay.in.Minutes)] <- mean(unclean_data$Arrival.Delay.in.Minutes, na.rm=TRUE)
 unclean_data$Flight.time.in.minutes[is.na(unclean_data$Flight.time.in.minutes)] <- median(unclean_data$Flight.time.in.minutes, na.rm=TRUE)
 
-#Summary after removing NA's 
-summary(unclean_data$Departure.Delay.in.Minutes)
-summary(unclean_data$Arrival.Delay.in.Minutes)
-summary(unclean_data$Flight.time.in.minutes)
-summary(unclean_data$Satisfaction)
-
 #Replacing the NA's with the mean
 unclean_data$Satisfaction[is.na(unclean_data$Satisfaction)] <- mean(unclean_data$Satisfaction, na.rm=TRUE)
 
-#Checking the values of the Dependent Variable after removing the NA's
-summary(unclean_data$Satisfaction)
 
 
 
