61e0c5c3e591a7c6e6854413f425c9aef4d36db0
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 11:54:19 -0400

    Saving preliminaryDataAnalysis.R

--- preliminaryDataAnalysis.R
+++ preliminaryDataAnalysis.R
@@ -1,2 +1,7 @@
 
 str(data)
+
+#Hypothesis - Male and Female passengers have significant differences in satisfaction.
+hist(data$Satisfaction)
+bp_satisByGender <- ggplot(data, aes(Gender,Satisfaction))+geom_boxplot(aes(col=Gender))+labs(title="Boxplot of Satisfaction by Gender")
+bp_satisByGender
