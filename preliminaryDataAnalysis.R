341741a6dc7ab50611a9c0806080f36e856eb2dd
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 12:07:02 -0400

    Saving preliminaryDataAnalysis.R

--- preliminaryDataAnalysis.R
+++ preliminaryDataAnalysis.R
@@ -3,5 +3,12 @@ library(ggplot2)
 str(data)
 
 
-#Hypothesis - Male and Female passengers have significant differences in satisfaction.
+#Hypothesis - Male and Female passengers have significant differences in 
+#satisfaction.
+t.test(data$Satisfaction ~ data$Gender)
+#  P-value < 2.2e-16 There is significant difference in satisfaction scores 
+#between male and female passengers, so we will be including data$Gender in the model.
+
+#Hypothesis - Male and Female passengers have significant differences in 
+#satisfaction.
 t.test(data$Satisfaction ~ data$Gender)
