68aa6a9b0f5512a2b0b65f85448aaa2950187237
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 13:13:28 -0400

    Saving preliminaryDataAnalysis.R

--- preliminaryDataAnalysis.R
+++ preliminaryDataAnalysis.R
@@ -2,18 +2,27 @@
 library(ggplot2)
 str(data)
 
-
-#Hypothesis - Male and Female customers have significant differences in 
+#Hypothesis1 - Male and Female customers have significant differences in 
 #satisfaction.
 t.test(data$Satisfaction ~ data$Gender)
 #  P-value < 2.2e-16 There is significant difference in satisfaction scores 
-#between male and female passengers, so we will be including data$Gender in the model.
+#between male and female passengers, so we will be including data$Gender in 
+#the model.
 
-#Hypothesis -Customers with different airline status have significant differences in 
-#satisfaction.
+#Hypothesis2 -Customers with different airline status have significant 
+#differences in satisfaction.
 model1<- aov(data$Satisfaction~data$Airline.Status)
 summary(model1)
 
 ## sig difference
 TukeyHSD(model1, conf.level = 0.99)
+#There are four AilineStatus, six possible pair-wise comparisons are obtained.
+#The results show that all the pairs of satisfaction are statistically 
+#significantly different from the Tukey’s Honestly Significant Differences.
+
+#Hypothesis3 -Customers with age have significant 
+#differences in satisfaction.
+
+summary(age)
+
 
