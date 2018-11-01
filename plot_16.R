61acaa5b980612e01024e7d2f4c8698dbc8760fc
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-31 23:58:04 -0400

    Saving plot_16.R

--- plot_16.R
+++ plot_16.R
@@ -1,7 +1,7 @@
 
 library(ggplot2)
 library(dplyr)
-satisfactioncount <- data %>% group_by(Orgin.City) %>% summarize(mean1 = mean(satisfaction))
+satisfactioncount <- data %>% group_by(Orgin.City) %>% summarize(mean1 = mean(Satisfaction))
 satisfactioncount <- as.data.frame(satisfactioncount)
 #plotting average satisfaction per gender vs customer satisfaction
 plot16 <- ggplot(satisfactioncount,aes(Orgin.City,mean1)) 
