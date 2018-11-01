db2b3ba7ed907e566aaeb0a8ba48870ac65dd76f
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 00:48:32 -0400

    Saving Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -1,15 +1,17 @@
 
 library(ggplot2)
-
+#day of month vs. satisfaction
+summary(data$Day.of.Month)
+dayofmonth <- ggplot(data=data, aes(x=Day.of.Month, y=Satisfaction)) + geom_bar(stat='identity')
 #flight date
 summary(data$Flight.date)
-flightdate <- ggplot(data=data, aes(x=Flight.date, y=Satisfaction)) +geom_bar(stat='identity')
+flightdatesat <- ggplot(data=data, aes(x=Flight.date, y=Satisfaction)) +geom_bar(stat='identity')
 
 #airline code
 summary(data$Airline.Code)
 
 counts <- table(data$Satisfaction, data$Airline.Code)     
-barplot(counts, main="Satisfaction depending on Airline Code",
+airlinecodesat <- barplot(counts, main="Satisfaction depending on Airline Code",
         col=c("darkblue","red", "yellow", "orange", "pink", "chartreuse", "brown1", "darkgoldenrod1", "burlywood3", "darkslategray2", "azure1", "bisque1", "blueviolet", "goldenrod4"), las=2,
         legend=rownames(counts), args.legend = list(x = "top", ncol=2, inset=c(0, 0), cex=.4))
 
@@ -17,9 +19,6 @@ barplot(counts, main="Satisfaction depending on Airline Code",
 summary(data$Airline.Name)
 
 counts2 <- table(data$Satisfaction, data$Airline.Name)     
-barplot(counts2, main="Satisfaction depending on Airline Name",
+airlinenamesat <- barplot(counts2, main="Satisfaction depending on Airline Name",
         col=c("darkblue","red", "yellow", "orange", "pink", "chartreuse", "brown1", "darkgoldenrod1", "burlywood3", "darkslategray2", "azure1", "bisque1", "blueviolet", "goldenrod4"), las=2,
         legend=rownames(counts2), args.legend = list(x = "top", ncol=2, inset=c(0, 0), cex=.4))
-#day of month vs. satisfaction
-summary(data$Day.of.Month)
-dayofmonth <- ggplot(data=data, aes(x=Day.of.Month, y=Satisfaction)) + geom_bar(stat='identity')
