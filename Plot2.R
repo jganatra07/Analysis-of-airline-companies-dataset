23e7e7c80b66105c4366867445691f6fc2d464d5
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-31 23:38:24 -0400

    Saving Plot2.R

--- Plot2.R
+++ Plot2.R
@@ -1,1 +1,15 @@
 
+
+str(data)
+
+library(ggplot2)
+
+#data$Satisfaction<-tolower(data$Satisfaction)
+
+agemean<-as.data.frame(agemean)
+
+#agesat<-ggplot(agemean,aes(Age,m1)) + geom_bar(stat="identity") + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+
+# ggplot(satmean,aes(Satisfaction,m1)) + geom_bar(stat="identity") + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+perflightsat <- ggplot(hotelf, aes(x=jitter(data$% of Flight with other Airlines), y=Satisfaction, colour= "red"))+ geom_point()
