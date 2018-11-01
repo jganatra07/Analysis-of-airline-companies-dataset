3e1e61082315be16d77c547c47808187d72dc805
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-31 23:51:33 -0400

    Saving plot_16.R

--- plot_16.R
+++ plot_16.R
@@ -1,3 +1,11 @@
 
 data$Orgin.City <- tolower(data$Orgin.City)
 us <- map_data("state")
+plot16 <- ggplot(data, aes(map_id = Orgin.City))
+#creating a map visualization
+plot16 <- plot16 + geom_map(map = us, aes(fill = factor(data$Satisfaction)))
+#defining the x and y axes values of the map
+#coord_map() handles the distortion and aspect ratio of the map
+plot16 <- plot16 + expand_limits(x = us$long, y = us$lat) + coord_map()
+#ggtitle() gives a title to the map
+biplot3
