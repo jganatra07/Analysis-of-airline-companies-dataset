dd63aebaa0f4926b1d8cca25590e7ddb2f6085ff
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 00:04:08 -0400

    Saving Plot2.R

--- Plot2.R
+++ Plot2.R
@@ -7,3 +7,6 @@ library(ggplot2)
 
 # make scatter plot of independent variable : X..of.Flight.with.other.Airlines and dependent variable : Satisfaction 
 perflightsat <- ggplot(data, aes(x=jitter(data$X..of.Flight.with.other.Airlines), y=Satisfaction, colour= "red"))+ geom_point()
+
+# make scatter plot of independent variable : X..of.Flight.with.other.Airlines and dependent variable : Satisfaction 
+typetravvelsat <- ggplot(data, aes(x=jitter(data$Type.of.Travel ), y=Satisfaction, colour= "red")) + geom_boxplot(fill="grey")
