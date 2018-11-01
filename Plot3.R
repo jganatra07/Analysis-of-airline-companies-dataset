784f6ff6b3419c63ee0764bd65c602e16642ca4b
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 00:42:43 -0400

    Saving Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -20,3 +20,6 @@ counts2 <- table(data$Satisfaction, data$Airline.Name)
 barplot(counts2, main="Satisfaction depending on Airline Name",
         col=c("darkblue","red", "yellow", "orange", "pink", "chartreuse", "brown1", "darkgoldenrod1", "burlywood3", "darkslategray2", "azure1", "bisque1", "blueviolet", "goldenrod4"), las=2,
         legend=rownames(counts2), args.legend = list(x = "top", ncol=2, inset=c(0, 0), cex=.4))
+#day of month vs. satisfaction
+summary(data$Day.of.Month)
+dayofmonth <- ggplot(data=data, aes(x=Day.of.Month, y=Satisfaction)) + geom_bar(stat='identity')
