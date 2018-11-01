859789ef19b48de8e411e877c095ef6bbdaf5bc4
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 12:04:17 -0400

    Saving preliminaryDataAnalysis.R

--- preliminaryDataAnalysis.R
+++ preliminaryDataAnalysis.R
@@ -4,8 +4,4 @@ str(data)
 
 
 #Hypothesis - Male and Female passengers have significant differences in satisfaction.
-#This line is for checking the distrubution of satisfaction to see if there is any outliers
-
-plot1_hist <- hist(data$Satisfaction)
-
 t.test(data$Satisfaction ~ data$Gender)
