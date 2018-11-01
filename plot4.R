684cc641a264efd1272db872c6e46df7948a0e38
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 14:20:51 -0400

    Saving plot4.R

--- plot4.R
+++ plot4.R
@@ -1,10 +1,11 @@
 
-#installing librarlibrary("ggplot2")
+#installing library
+library("ggplot2")
 library("ggmap")
 library("dplyr")
-
+#calculating mean and discarding NA values
 mean(data$Satisfaction,na.rm=TRUE)
-
+#
 data$Satisfaction[is.na(data$Satisfaction)] <- 3.5 #closest to mean
 fltmean1 <- data %>% group_by(Orgin.City) %>% summarize(mean1 = mean(Satisfaction))
 fltmean1 <- as.data.frame(fltmean1)
@@ -30,26 +31,12 @@ originCity <- ggplot(us, aes(x=long, y=lat)) + expand_limits(x = us$long, y = us
 originCity <- originCity + geom_polygon() + coord_map()
 originCity <- originCity + geom_point(data=latlon, aes(x=lon, y=lat, size=mean1), color="orange")
 
-osmean <- data %>%
+osmean <- unclean_data %>%
   group_by(Origin.State) %>%
-  summarize(m1 = mean(Satisfaction))
-
+  summarize(m1 = mean(as.numeric(Satisfaction)))
 osmean<-as.data.frame(osmean)
 osmean
 originState <- ggplot(osmean, aes(x=Origin.State, y=m1)) + geom_col()
 originState <- originState + ggtitle("Bar chart of customer satisfaction per Origin state")
 originState <- originState + theme(axis.text.x = element_text(angle = 90, hjust = 1))
-
-#destinationState <- ggplot(data, aes(x=Destination.State, y=Satisfaction)) + geom_col()
-#destinationState <- destinationState + ggtitle("Bar chart of customer satisfaction per Destination state")
-#destinationState <- destinationState + theme(axis.text.x = element_text(angle = 90, hjust = 1))
-
-satmean <- data %>%
-  group_by(Satisfaction) %>%
-  summarize(m2 = mean(Scheduled.Departure.Hour))
-
-sdhmean<-as.data.frame(satmean)
-
-sdh <- ggplot(satmean,aes(Satisfaction,m2)) + geom_bar(stat="identity") + theme(axis.text.x = element_text(angle = 90, hjust = 1))
-sdh
-
+originState
