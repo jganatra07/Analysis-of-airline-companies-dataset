d9471bccd49955efc726bc10c43761a8e9a8695d
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 11:53:33 -0400

    Saving Plot5.R

--- Plot5.R
+++ Plot5.R
@@ -1,4 +1,5 @@
 
+library(ggplot2)
 str(data)
 departure_delay__plot<-ggplot(data,aes(x=data$Departure.Delay.in.Minutes,y=data$Satisfaction))+geom_count(color="red")
 departure_delay__plot<-departure_delay__plot+labs(x="Departure Delay (minutes)",y="Customer Satisfaction")
