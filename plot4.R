4e212909f536e2833870fe24b80aa2e903091286
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 02:43:28 -0400

    Saving plot4.R

--- plot4.R
+++ plot4.R
@@ -6,7 +6,7 @@ library(dplyr)
 mean(data$Satisfaction,na.rm=TRUE)
 
 data$Satisfaction[is.na(clean_data$Satisfaction)] <- 3.5 #closest to mean
-fltmean1 <- clean_data %>% group_by(Orgin.City) %>% summarize(mean1 = mean(Satisfaction))
+fltmean1 <- data %>% group_by(Orgin.City) %>% summarize(mean1 = mean(Satisfaction))
 fltmean1 <- as.data.frame(fltmean1)
 fltmean1$place <- toString(paste(clean_data$Orgin.City,",",clean_data$Origin.State))
 fltmean1 <- as.data.frame(fltmean1)
@@ -28,7 +28,5 @@ latlon <- as.data.frame(cbind(lon,lat,fltmean1))
 
      
 
-ggplot(us, aes(x=long, y=lat)) + expand_limits(x = us$long, y = us$lat) +
-  geom_polygon() +
-  coord_map() +
-  geom_point(data=latlon, aes(x=lon, y=lat, size=mean1), color="orange")
+originCity <- ggplot(us, aes(x=long, y=lat)) + expand_limits(x = us$long, y = us$lat) + geom_point(data=latlon, aes(x=lon, y=lat, size=mean1), color="orange")
+originCity <- originCity + geom_polygon() + coord_map()
