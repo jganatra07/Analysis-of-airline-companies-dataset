9ea1dbbbaa32a0125ad864d645194a166b6460e6
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-08 11:31:00 -0500

    Saving correlation.R

--- correlation.R
+++ correlation.R
@@ -1,2 +1,14 @@
 
 str(data)
+
+data_num = data[['Satisfaction','Age','Price.Sensitivity', 
+                       'Year.of.First.Flight', 'No.of.Flights.p.a.',                     
+                       'X..of.Flight.with.other.Airlines', 
+                       'No..of.other.Loyalty.Cards',
+                       'Shopping.Amount.at.Airport', 
+                       'Eating.and.Drinking.at.Airport', 
+                       'Day.of.Month',
+                        'Scheduled.Departure.Hour',
+                        'Flight.time.in.minutes',
+                        'Flight.Distance ']].copy()
+data_num.head()
