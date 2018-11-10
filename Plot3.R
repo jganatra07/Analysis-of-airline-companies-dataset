64e641b9321d686f540300c14caab1e21bdd4a2c
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-10 12:46:37 -0500

    Saving Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -12,6 +12,7 @@ summary(data$Airline.Code)
 counts <- table(data$Satisfaction, data$Airline.Code) 
 
 airlinecodesat <- barplot(counts, main="Satisfaction depending on Airline Code", col=c("darkblue","red", "yellow", "orange", "pink", "chartreuse", "brown1", "darkgoldenrod1", "burlywood3", "darkslategray2", "azure1", "bisque1", "blueviolet", "goldenrod4"), las=2, legend=rownames(counts), args.legend = list(x = "top", ncol=2, inset=c(0, 0), cex=.4))
+airlinecodesat
 
 #airline name
 summary(data$Airline.Name)
@@ -19,3 +20,4 @@ summary(data$Airline.Name)
 counts2 <- table(data$Satisfaction, data$Airline.Name)
 
 airlinenamesat <- barplot(counts2, main="Satisfaction depending on Airline Name", col=c("darkblue","red", "yellow", "orange", "pink", "chartreuse", "brown1", "darkgoldenrod1", "burlywood3", "darkslategray2", "azure1", "bisque1", "blueviolet", "goldenrod4"), las=2, legend=rownames(counts2), args.legend = list(x = "top", ncol=2, inset=c(0, 0), cex=.4))
+airlinenamesat
