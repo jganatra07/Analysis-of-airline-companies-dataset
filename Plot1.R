6e26e8d7d17fd7e44c91cae8a388c32d18067aab
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-14 20:29:39 -0500

    force updated content when merge failed

--- Plot1.R
+++ Plot1.R
@@ -5,7 +5,6 @@ library(ggplot2)
 library(dplyr)
 #plot(data$Age, data$Satisfaction)
 
-#data$Satisfaction<-tolower(data$Satisfaction)
 
 #Plot of Age vs Avg. Satisfaction
 satmean <- data %>%
@@ -13,19 +12,15 @@ satmean <- data %>%
   summarize(m1 = mean(Age))
 
 agemean<-as.data.frame(satmean)
-print("structure of agemean")
-str(agemean)
+
 agesat<-ggplot(agemean,aes(Satisfaction,m1)) + geom_bar(stat="identity") + theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
 #Plotting Satisfaction against the Flight Date
-#data$Flight.date <- as.factor(data$Flight.date)
-#data$Satisfaction <- as.numeric(data$Satisfaction)
 flightdate <- data %>%
   group_by(Flight.date) %>%
   summarize(m2 = mean(Satisfaction))
-  
-print("structure of flightdate")
-str(flightdate)
+
+flightdate<-as.data.frame(flightdate)
 
 flight_date<-ggplot(flightdate, aes(Flight.date,m2, group=1))+geom_line()+labs(x="From 1st Jan to 31st March 2014", y="Mean Satisfaction")
 
@@ -34,6 +29,4 @@ flight_date<-ggplot(flightdate, aes(Flight.date,m2, group=1))+geom_line()+labs(x
 air_status<-ggplot(data, aes(Airline.Status, Satisfaction))+geom_boxplot()+labs(x="Airline Status", y="Satisfaction")
 
 #Plotting Gender against Satisfaction
-#data1 <- data
-#data1$genderFactor <- as.factor(data1$Gender)
 gender_sat<-ggplot(data, aes(Gender, Satisfaction))+geom_boxplot()+labs(x="Gender", y="Satisfaction")
