4f308086728b9c68b044eb8f77e71078f5cfa6fb
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-30 10:50:44 -0500

    Saving modelTesting_lm.R

--- modelTesting_lm.R
+++ modelTesting_lm.R
@@ -23,6 +23,7 @@ cor_2
 cor_m<-cor(nu_data)
 head(round(cor_m,2))
 cor <- ggcorrplot(cor_m)
+cor
 
 # output-- plot_cor_m
 #from the correlation matrix, we can see that the below pairs of variables are 
