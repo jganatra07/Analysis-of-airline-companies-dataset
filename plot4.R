6121131570f79915824b4b89bb05361279242670
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-05 01:53:02 -0500

    Saving plot4.R

--- plot4.R
+++ plot4.R
@@ -29,8 +29,8 @@ latlon <- as.data.frame(cbind(lon,lat,fltmean1))
 
 originCity <- ggplot(us, aes(x=long, y=lat)) + expand_limits(x = us$long, y = us$lat) 
 originCity <- originCity + geom_polygon() + coord_map()
-originCity <- originCity + geom_point(data=latlon, aes(x=lon, y=lat, size=mean1), color="orange")
-
+originCity <- originCity + geom_point(data=latlon, aes(x=lon, y=lat, size=0.5), color="orange")
+originCity
 #Calculating mean of satisfaction and grouping it by Origin state
 osmean <- data %>%
   group_by(Origin.State) %>%
