de7ede18e0bbe129c57c7582366506fe41f95e5f
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-05 16:23:25 -0500

    Saving Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -13,7 +13,7 @@ satmean <- data %>%
 
 agemean<-as.data.frame(satmean)
 
-agesat<-ggplot(agemean,aes(Satisfaction,m1)) + geom_bar(stat="identity") + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+agesat<-ggplot(agemean,aes(Satisfaction,m1)) + geom_bar(stat="identity") + theme(axis.text.x = element_text(angle = 90, hjust = 1)+labs(x="Satisfaction", y="Average Age"))
 
 #Plotting Satisfaction against the Flight Date
 flightdate <- data %>%
