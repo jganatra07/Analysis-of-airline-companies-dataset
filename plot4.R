fa11c54fa9c17cb39545fb85ee12af7ca3cb101f
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-05 10:32:26 -0500

    Saving plot4.R

--- plot4.R
+++ plot4.R
@@ -5,7 +5,6 @@ library("ggmap")
 library("dplyr")
 #calculating mean and discarding NA values
 mean(data$Satisfaction,na.rm=TRUE)
-#
 data$Satisfaction[is.na(data$Satisfaction)] <- 3.5 #closest to mean
 fltmean1 <- data %>% group_by(Orgin.City) %>% summarize(mean1 = mean(Satisfaction))
 fltmean1 <- as.data.frame(fltmean1)
@@ -15,7 +14,7 @@ us <- map_data("state")
 
 lat <- data.frame()
 lon <- data.frame()
-#get the latitude and longitude of NYC from the datasciencetoolkit
+#getting the latitude and longitude of NYC from the datasciencetoolkit
 for(i in 1:nrow(fltmean1)){
 
 x <- geocode(as.character(fltmean1[i,1]),source="dsk")
@@ -26,7 +25,7 @@ head(lat)
 head(lon)
 
 latlon <- as.data.frame(cbind(lon,lat,fltmean1))
-
+#plotting the map for mean of satisfaction in origin city
 originCity <- ggplot(us, aes(x=long, y=lat)) + expand_limits(x = us$long, y = us$lat) 
 originCity <- originCity + geom_polygon() + coord_map()
 originCity <- originCity + geom_point(data=latlon, aes(x=lon, y=lat, size=0.05), color="orange")
