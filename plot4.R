ba1d0f149677e224369dce340f6f48faade15d5e
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-05 14:26:30 -0500

    Saving plot4.R

--- plot4.R
+++ plot4.R
@@ -47,25 +47,25 @@ originState
 #calculating mean and discarding NA values
 mean(data$Satisfaction,na.rm=TRUE)
 data$Satisfaction[is.na(data$Satisfaction)] <- 3.5 #closest to mean
-fltmean1 <- data %>% group_by(Destination.City) %>% summarize(mean1 = mean(Satisfaction))
-fltmean1 <- as.data.frame(fltmean1)
-fltmean1$place <- toString(paste(data$Destination.City,",",data$Destination.State))
-fltmean1 <- as.data.frame(fltmean1)
+fltmean2 <- data %>% group_by(Destination.City) %>% summarize(mean2 = mean(Satisfaction))
+fltmean2 <- as.data.frame(fltmean2)
+fltmean2$place <- toString(paste(data$Destination.City,",",data$Destination.State))
+fltmean2 <- as.data.frame(fltmean2)
 us <- map_data("states")
 
 lat <- data.frame()
 lon <- data.frame()
 #getting the latitude and longitude of NYC from the datasciencetoolkit
-for(i in 1:nrow(fltmean1)){
+for(i in 1:nrow(fltmean2)){
   
-  x <- geocode(as.character(fltmean1[i,1]),source="dsk")
+  x <- geocode(as.character(fltmean2[i,1]),source="dsk")
   lon[i,1] <- x[1]
   lat[i,1] <- x[2]
 }
 head(lat)
 head(lon)
 
-latlon <- as.data.frame(cbind(lon,lat,fltmean1))
+latlon <- as.data.frame(cbind(lon,lat,fltmean2))
 #plotting the map for mean of satisfaction in destination city
 DestCity <- ggplot(us, aes(x=long, y=lat)) + expand_limits(x = us$long, y = us$lat) 
 DestCity <- DestCity + geom_polygon() + coord_map()
