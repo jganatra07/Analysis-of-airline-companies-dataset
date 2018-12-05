3a0b5f703731ff5b83f6fc1cd44b254264a3d7de
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-05 10:07:52 -0500

    Saving modelTesting_lm.R

--- modelTesting_lm.R
+++ modelTesting_lm.R
@@ -95,4 +95,59 @@ summary(model3)
 model4 <- lm(Satisfaction ~ . -Flight.time.in.minutes -Flight.Distance -Arrival.Delay.in.Minutes -Departure.Delay.in.Minutes -No..of.other.Loyalty.Cards -Flight.date -Day.of.Month -Airline.Code -Airline.Name -Year.of.First.Flight -Eating.and.Drinking.at.Airport -Orgin.City -Origin.State -Destination.City -Destination.State -X..of.Flight.with.other.Airlines, data = data)
 summary(model4)
 
-# ADJUSTED R-SUQRED 0.4443
+# ADJUSTED R-SUQRED 0.4069 with 11 predictors
+
+model5 <- lm(Satisfaction ~ . -Shopping.Amount.at.Airport -Flight.time.in.minutes -Flight.Distance -Arrival.Delay.in.Minutes -Departure.Delay.in.Minutes -No..of.other.Loyalty.Cards -Flight.date -Day.of.Month -Airline.Code -Airline.Name -Year.of.First.Flight -Eating.and.Drinking.at.Airport -Orgin.City -Origin.State -Destination.City -Destination.State -X..of.Flight.with.other.Airlines, data = data)
+summary(model5)
+# ADJUSTED R-SUQRED 0.4069 with 10 predictors
+
+model6 <- lm(Satisfaction ~ . -Flight.cancelled -Shopping.Amount.at.Airport -Flight.time.in.minutes -Flight.Distance -Arrival.Delay.in.Minutes -Departure.Delay.in.Minutes -No..of.other.Loyalty.Cards -Flight.date -Day.of.Month -Airline.Code -Airline.Name -Year.of.First.Flight -Eating.and.Drinking.at.Airport -Orgin.City -Origin.State -Destination.City -Destination.State -X..of.Flight.with.other.Airlines, data = data)
+summary(model6)
+# ADJUSTED R-SUQRED 0.4063 with 9 predictors
+
+model7 <- lm(Satisfaction ~ . -Price.Sensitivity -Flight.cancelled -Shopping.Amount.at.Airport -Flight.time.in.minutes -Flight.Distance -Arrival.Delay.in.Minutes -Departure.Delay.in.Minutes -No..of.other.Loyalty.Cards -Flight.date -Day.of.Month -Airline.Code -Airline.Name -Year.of.First.Flight -Eating.and.Drinking.at.Airport -Orgin.City -Origin.State -Destination.City -Destination.State -X..of.Flight.with.other.Airlines, data = data)
+summary(model7)
+# ADJUSTED R-SUQRED 0.4057 with 8 predictors
+
+
+model8 <- lm(Satisfaction ~ .-Arrival.Delay.greater.5.Mins -Price.Sensitivity -Flight.cancelled -Shopping.Amount.at.Airport -Flight.time.in.minutes -Flight.Distance -Arrival.Delay.in.Minutes -Departure.Delay.in.Minutes -No..of.other.Loyalty.Cards -Flight.date -Day.of.Month -Airline.Code -Airline.Name -Year.of.First.Flight -Eating.and.Drinking.at.Airport -Orgin.City -Origin.State -Destination.City -Destination.State -X..of.Flight.with.other.Airlines, data = data)
+summary(model8)
+# ADJUSTED R-SUQRED 0.4057 --> 38.79 with 7 predictors, so I decided to keep arrival delay greater 5 mins
+
+model9 <- lm(Satisfaction ~ .-Scheduled.Departure.Hour -Price.Sensitivity -Flight.cancelled -Shopping.Amount.at.Airport -Flight.time.in.minutes -Flight.Distance -Arrival.Delay.in.Minutes -Departure.Delay.in.Minutes -No..of.other.Loyalty.Cards -Flight.date -Day.of.Month -Airline.Code -Airline.Name -Year.of.First.Flight -Eating.and.Drinking.at.Airport -Orgin.City -Origin.State -Destination.City -Destination.State -X..of.Flight.with.other.Airlines, data = data)
+summary(model9)
+
+# ADJUSTED R-SUQRED 0.4057 -->40.54 with 7 predictors
+
+model10 <- lm(Satisfaction ~ .-No.of.Flights.p.a. -Scheduled.Departure.Hour -Price.Sensitivity -Flight.cancelled -Shopping.Amount.at.Airport -Flight.time.in.minutes -Flight.Distance -Arrival.Delay.in.Minutes -Departure.Delay.in.Minutes -No..of.other.Loyalty.Cards -Flight.date -Day.of.Month -Airline.Code -Airline.Name -Year.of.First.Flight -Eating.and.Drinking.at.Airport -Orgin.City -Origin.State -Destination.City -Destination.State -X..of.Flight.with.other.Airlines, data = data)
+summary(model10)
+
+# ADJUSTED R-SUQRED 0.4054 --->40.29 with 6 predictors
+
+model11 <- lm(Satisfaction ~ .-Class -No.of.Flights.p.a. -Scheduled.Departure.Hour -Price.Sensitivity -Flight.cancelled -Shopping.Amount.at.Airport -Flight.time.in.minutes -Flight.Distance -Arrival.Delay.in.Minutes -Departure.Delay.in.Minutes -No..of.other.Loyalty.Cards -Flight.date -Day.of.Month -Airline.Code -Airline.Name -Year.of.First.Flight -Eating.and.Drinking.at.Airport -Orgin.City -Origin.State -Destination.City -Destination.State -X..of.Flight.with.other.Airlines, data = data)
+summary(model11)
+
+# ADJUSTED R-SUQRED 0.4029 --> 40.23 with 5 predictors
+
+model12 <- lm(Satisfaction ~ .-Age -Class -No.of.Flights.p.a. -Scheduled.Departure.Hour -Price.Sensitivity -Flight.cancelled -Shopping.Amount.at.Airport -Flight.time.in.minutes -Flight.Distance -Arrival.Delay.in.Minutes -Departure.Delay.in.Minutes -No..of.other.Loyalty.Cards -Flight.date -Day.of.Month -Airline.Code -Airline.Name -Year.of.First.Flight -Eating.and.Drinking.at.Airport -Orgin.City -Origin.State -Destination.City -Destination.State -X..of.Flight.with.other.Airlines, data = data)
+summary(model12)
+
+# ADJUSTED R-SUQRED 0.4057 -->40.54  7  --->40.29 6  --- 40.23 5 -- 40.13 4
+
+model13 <- lm(Satisfaction ~ .-Gender -Age -Class -No.of.Flights.p.a. -Scheduled.Departure.Hour -Price.Sensitivity -Flight.cancelled -Shopping.Amount.at.Airport -Flight.time.in.minutes -Flight.Distance -Arrival.Delay.in.Minutes -Departure.Delay.in.Minutes -No..of.other.Loyalty.Cards -Flight.date -Day.of.Month -Airline.Code -Airline.Name -Year.of.First.Flight -Eating.and.Drinking.at.Airport -Orgin.City -Origin.State -Destination.City -Destination.State -X..of.Flight.with.other.Airlines, data = data)
+summary(model13)
+# ADJUSTED R-SUQRED 0.4057 -->40.54  7  --->40.29 6  --- 40.23 5 -- 40.13 4  --- 39.46 3 
+
+m <- lm(formula = Satisfaction ~ Airline.Status + Type.of.Travel, data = data)
+summary(m)
+# ADJUSTED R-SUQRED 37.73
+
+m2 <- lm(formula = Satisfaction ~ Airline.Status + Type.of.Travel + Arrival.Delay.greater.5.Mins, data = data)
+summary(m2)
+# ADJUSTED R-SUQRED 39.46
+
+m3 <- lm(formula = Satisfaction ~ Airline.Status + Type.of.Travel + Arrival.Delay.greater.5.Mins, data = data)
+summary(m3)
+# ADJUSTED R-SUQRED for final model was 39.46 
+
+
