bf616cb7d06b7eef675aae998759707236d82b7c
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-05 20:38:23 -0500

    Saving Logistic_Regression.R

--- Logistic_Regression.R
+++ Logistic_Regression.R
@@ -16,6 +16,8 @@ library(e1071)
 
 #Creating a single variable that can show the absolute value of the delay, instead of having two variable.
 unclean_data$EffectiveDelay<-abs(unclean_data$Departure.Delay.in.Minutes-unclean_data$Arrival.Delay.in.Minutes)
+
+#Removing the two variables
 unclean_data$Departure.Delay.in.Minutes<-NULL
 unclean_data$Arrival.Delay.in.Minutes<-NULL
 
