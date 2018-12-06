a5309c152caf6759e648a75701c21da6a5ad20d8
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-05 20:37:13 -0500

    Saving Logistic_Regression.R

--- Logistic_Regression.R
+++ Logistic_Regression.R
@@ -1,1 +1,5 @@
 
+unclean_data$EffectiveDelay<-abs(unclean_data$Departure.Delay.in.Minutes-unclean_data$Arrival.Delay.in.Minutes)
+unclean_data$Departure.Delay.in.Minutes<-NULL
+unclean_data$Arrival.Delay.in.Minutes<-NULL
+
