863bf30e61c6edd731917d6deeb8ddf6e604bb34
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-05 20:38:12 -0500

    Saving Logistic_Regression.R

--- Logistic_Regression.R
+++ Logistic_Regression.R
@@ -1,4 +1,20 @@
 
+library(jtools)
+#install.packages('pscl')
+library(pscl)
+#install.packages("broom")
+library(broom)
+#install.packages("ggstance")
+library(ggstance)
+#install.packages("effects")
+library(effects)
+#install.packages("lattice")
+#install.packages("caret")
+library(caret)
+#install.packages("e1071")
+library(e1071)
+
+#Creating a single variable that can show the absolute value of the delay, instead of having two variable.
 unclean_data$EffectiveDelay<-abs(unclean_data$Departure.Delay.in.Minutes-unclean_data$Arrival.Delay.in.Minutes)
 unclean_data$Departure.Delay.in.Minutes<-NULL
 unclean_data$Arrival.Delay.in.Minutes<-NULL
